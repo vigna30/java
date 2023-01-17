644ad4bbf6d61da485b8d6471a845828b0e39bb8
vigna30 <83338501+vigna30@users.noreply.github.com>
2023-01-17 10:28:02 +0530

    Update Revservestring.java

--- Revservestring.java
+++ Revservestring.java
@@ -1,10 +1,36 @@
-public class Revservestring {
-    public static void main(String args[]) {
-      String source = "Hello";
+import java.io.*; 
 
-for (String part : source.split(" ")) {
-    System.out.print(new StringBuilder(part).reverse().toString());
-    System.out.print(" ");
-}
-}
-}
+import java.util.Scanner; 
+
+  
+
+class reversestring { 
+
+    public static void main (String[] args) { 
+
+        
+
+        String str= "vigna", nstr=""; 
+
+        char ch; 
+
+        
+
+      System.out.print("Original word: "); 
+
+      System.out.println("vigna"); 
+
+        
+
+      for (int i=0; i<str.length(); i++) 
+
+      { 
+
+        ch= str.charAt(i);
+        nstr= ch+nstr; 
+      } 
+
+      System.out.println("Reversed word: "+ nstr); 
+
+    } 
+} 
